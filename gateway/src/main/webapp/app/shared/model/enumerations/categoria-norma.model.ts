837af43d95a07b514a76d2565bc49d15e3fa891a
Luiz Kill <luiz.kill@cesan.com.br>
2020-02-13 13:45:05 -0300

    Remove a entidade CategoriaNorma

--- gateway/src/main/webapp/app/shared/model/enumerations/categoria-norma.model.ts
+++
@@ -1,5 +0,0 @@
-export const enum CategoriaNorma {
-  QUALIDADE,
-  AMBIENTAL,
-  SEGURANCA
-}
--- sgq/src/main/java/com/lzkill/sgq/domain/enumeration/CategoriaNorma.java
+++
@@ -1,8 +0,0 @@
-package com.lzkill.sgq.domain.enumeration;
-
-/**
- * The CategoriaNorma enumeration.
- */
-public enum CategoriaNorma {
-    QUALIDADE, AMBIENTAL, SEGURANCA
-}
