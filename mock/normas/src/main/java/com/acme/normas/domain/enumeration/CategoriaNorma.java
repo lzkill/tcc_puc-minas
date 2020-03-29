a495264040dd4374cb49acdc679aec3230f490c8
Luiz <luizkill@gmail.com>
2020-03-29 11:39:33 -0300

    Adiciona os textos do documento de arquitetura até a seção 5.1

--- mock/normas/src/main/java/com/acme/normas/domain/enumeration/CategoriaNorma.java
+++ mock/normas/src/main/java/com/acme/normas/domain/enumeration/CategoriaNorma.java
@@ -4,5 +4,5 @@ package com.acme.normas.domain.enumeration;
  * The CategoriaNorma enumeration.
  */
 public enum CategoriaNorma {
-    QUALIDADE, AMBIENTAL, SEGURANCA
+    QUALIDADE, AMBIENTAL, SEGURANCA, TECNICA, RECURSOS_HUMANOS
 }
